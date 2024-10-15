8f4e6350646c7f7cb51bd477461d7fc7a43674a8
Chaicoo <gabriellimma27@gmail.com>
2024-10-15 00:09:39 -0300

    feat: improve the login screen background

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white dark:bg-gray-900 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px] dark:bg-[linear-gradient(to_right,#ffffff0a_1px,transparent_1px),linear-gradient(to_bottom,#ffffff0a_1px,transparent_1px)]">
       <Navbar />
       <Hero />
       <ScrollToTop />
-    </>
+    </div>
   );
 }
 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,10 +2,12 @@ import { LoginCard } from "./LoginForm";
 
 export const Hero = () => {
   return (
-    <>
+    <div className="relative">
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white dark:bg-gray-900 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px] dark:bg-[linear-gradient(to_right,#ffffff0a_1px,transparent_1px),linear-gradient(to_bottom,#ffffff0a_1px,transparent_1px)]"></div>
+
       <section className="container grid lg:grid-cols-2 items-center py-20 md:py-32 gap-10">
-        <div className="text-center lg:text-start space-y-6 order-1 lg:order-none">
-          <main className="text-4xl md:text-6xl font-bold">
+        <div className="text-center lg:text-start space-y-6">
+          <main className="text-5xl md:text-6xl font-bold">
             <h1 className="inline">
               <span className="inline bg-gradient-to-r from-green-400 to-blue-500 text-transparent bg-clip-text">
                 Min Social
@@ -13,7 +15,7 @@ export const Hero = () => {
             </h1>
           </main>
 
-          <p className="text-lg md:text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
+          <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
             Mini rede social com layout inspirado no Twitter, criada com ReactJs, ShadCn, Laravel e MySql.
           </p>
         </div>
@@ -36,6 +38,6 @@ export const Hero = () => {
           </a>
         </h3>
       </section>
-    </>
+    </div>
   );
 };
